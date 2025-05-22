ebda609af7ec7b6792373586df01bbc21c4198a7
Saulo Ferro Maciel <102975817+Saulo-Ferro-Maciel@users.noreply.github.com>
2025-05-22 17:14:21 -0300

    Create Python Program to Calculate Grade Averages.py

---
+++ Introduction to Python/Python Program to Calculate Grade Averages.py
@@ -0,0 +1,27 @@
+controle = 1
+while controle != 0:
+    pergunta = input("Quantas notas a serem adicionadas?\nR: ")
+    
+    try:
+        nota = 0
+        notaString = ""
+        auxiliar = int(pergunta)
+        listaNotas = []
+        
+        for i in range(auxiliar):
+            pergunta = float(input(f"Digite o valor da {i+1}ª nota: "))
+            listaNotas.append(pergunta)
+            nota += listaNotas[i]
+            if i != auxiliar-1:
+                notaString += f"{listaNotas[i]} - "
+            else:
+                notaString += f"{listaNotas[i]}."
+        media = nota/auxiliar
+        
+        print(f"\nFora adicionadas {auxiliar} notas.\nAs notas adicionas fora {notaString}\nA média das notas é: {media}")
+    except:
+        print("Por favor valores digite válidos")
+    
+    pregunta = input("Deseja sair? [sim/não] ")
+    if pregunta[0].lower() == "s":
+        controle = 0
